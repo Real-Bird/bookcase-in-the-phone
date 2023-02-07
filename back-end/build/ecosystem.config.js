ed5c821a295ee62232cb9ac4ef377b990b40e933
Real-Bird <marbil815@gmail.com>
2023-02-08 03:10:22 +0900

    test: deploy trying 7
    23-02-08

--- back-end/build/ecosystem.config.js
+++ back-end/build/ecosystem.config.js
@@ -2,10 +2,9 @@ module.exports = {
   apps: [
     {
       name: "bip",
-      script: "./index.js",
+      script: "build/index.js",
       instances: 0,
       exec_mode: "cluster",
-      wait_ready: true,
       listen_timeout: 50000,
       kill_timeout: 5000,
     },
