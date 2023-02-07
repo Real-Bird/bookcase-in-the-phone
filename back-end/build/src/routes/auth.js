1202fe8abe4436f732742ab4ef36e511de0d8ea8
Real-Bird <marbil815@gmail.com>
2023-02-08 07:43:19 +0900

    test: deploy login test 2
    23-02-08

--- back-end/build/src/routes/auth.js
+++ back-end/build/src/routes/auth.js
@@ -6,7 +6,14 @@ const authCtrl = require("../controller/auth");
 router.get("/login/success", authCtrl.login);
 router.get("/logout", authCtrl.logout);
 router.get("/google", authCtrl.reqGoogle);
-router.get("/google/callback", authCtrl.callbackGoogle);
+router.get(
+  "/google/callback",
+  (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  },
+  authCtrl.callbackGoogle
+);
 router.get("/check", authCtrl.check);
 
 exports.router = router;
