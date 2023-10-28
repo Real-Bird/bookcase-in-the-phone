6b399cacf08a0dc11df548df03015d3a33b90677
Real-Bird <marbil815@gmail.com>
2023-10-28 17:28:48 +0900

    fix: check in memory cache using ref
    23-10-28

--- src/libs/hooks/useAuthGuard.ts
+++ src/libs/hooks/useAuthGuard.ts
@@ -1,22 +1,30 @@
 import { checkedUser } from "@api/auth";
 import { useUserDispatch } from "@libs/userContextApi";
-import { inMemoryCache } from "main";
-import { useCallback, useLayoutEffect } from "react";
+import { useCallback, useLayoutEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const useAuthGuard = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const userDispatch = useUserDispatch();
+  const inMemoryCache = useRef<InMemoryCache>({});
+
+  const checkedPathCache = (pathname: string) => {
+    const pathCache = inMemoryCache.current.pathCache;
+    if (pathCache && pathCache[pathname]?.expired > Date.now()) {
+      return true;
+    }
+    return false;
+  };
 
   const handleCheckedUser = useCallback(async () => {
     const res = await checkedUser();
     if (!res || !res.isLogged) {
-      inMemoryCache.isLogged = true;
+      inMemoryCache.current.isLogged = true;
       navigate("/login", { replace: true });
       return;
     }
-    inMemoryCache.isLogged = false;
+    inMemoryCache.current.isLogged = false;
     if (res.isLogged && pathname === "/login") {
       navigate("/", { replace: true });
       return;
@@ -25,13 +33,26 @@ export const useAuthGuard = () => {
   }, [pathname]);
 
   useLayoutEffect(() => {
-    if (inMemoryCache.isLogged) {
+    if (inMemoryCache.current.isLogged) {
       navigate("/login", {
         replace: true,
       });
       return;
     }
-    handleCheckedUser();
+    if (!checkedPathCache(pathname)) {
+      handleCheckedUser();
+      inMemoryCache.current.pathCache = {
+        ...inMemoryCache.current.pathCache,
+        [pathname]: { expired: Date.now() + 1000 * 60 * 60 },
+      };
+    }
+    console.log(inMemoryCache.current);
     return () => window.localStorage.removeItem("googleLoginComplete");
   }, [pathname]);
 };
+
+type PathCache = Record<string, { expired: number }>;
+interface InMemoryCache {
+  isLogged?: boolean;
+  pathCache?: PathCache;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 import "./styles/global.css";
 import FetchUserDataProvider from "@libs/userContextApi";
 
-export let inMemoryCache = { isLogged: false };
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <FetchUserDataProvider>
-    <RouterProvider router={router} />
-  </FetchUserDataProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <FetchUserDataProvider>
+      <RouterProvider router={router} />
+    </FetchUserDataProvider>
+  </React.StrictMode>
 );
